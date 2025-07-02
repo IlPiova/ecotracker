05b77c77baa7e28a156f86546d495b49556c8ecc
Cristian <piovani.cp@gmail.com>
2025-07-02 13:08:25 +0200

    Eliminati errori di layout

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.iksiclf4b5g"
+    "revision": "0.0qq2hcib848"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
